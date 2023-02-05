a07bc943202f92d3f3fe5500aa9354910cf5eed1
Igor <kravchenkoigor010687@gmail.com>
2023-02-05 17:24:56 +0100

    Create 2 folder with the HomeWork

---
+++ task5_HomeWork2/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние 
+// между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.Clear();
+Console.WriteLine("Введите координаты.")
+Console.WriteLine("Введите координаты Х1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Z1: ");
+double z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты X2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Y2: ");
+double y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Z2: ");
+double z2 = double.Parse(Console.ReadLine());
+double result =  Math.Pow((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) * 1.0), 0.5);
+Console.WriteLine($"Расстояние между точками равно - {result} .");
