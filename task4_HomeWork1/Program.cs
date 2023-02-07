43fcb1c6c5c024a491ec135ab0c19abe3138d525
Igor <kravchenkoigor010687@gmail.com>
2023-02-07 19:45:40 +0100

    Save all

--- task4_HomeWork1/Program.cs
+++ task4_HomeWork1/Program.cs
@@ -21,6 +21,27 @@ if(text != finalText)
 }
 
 
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int len = number.Length;
+
+// if (len == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} - Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - НЕ палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+// }
+// Нашел в интернете.Тоже работает, но мне кажется что первый способ который разбирали на уроке применим к любой длинне числа,
+//  и соответственно лучше.Только вот вопрос какой лучше писать на собеседовании? 
 
 
 
