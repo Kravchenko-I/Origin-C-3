18596f4fdc47cd54b0caa2c701014a8687e0769a
Igor <kravchenkoigor010687@gmail.com>
2023-02-07 19:38:34 +0100

    Save all

---
+++ task4_HomeWork1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли 
+// оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.Clear();
+Console.WriteLine("Введите число");
+string text =Console.ReadLine();
+char[] userInsert =text.ToCharArray();
+Array.Reverse(userInsert);
+string finalText = new String(userInsert);
+if(text == finalText)
+{
+   Console.WriteLine("Число палиндром.");
+}
+if(text != finalText)
+{
+    Console.WriteLine("Число не палиндром.");
+}
+
+
+
+
+
+
+
